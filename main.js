3f784e01b99c3124831ffda7a19866c1d7b2d824
paulodelosrey <135145158+paulodelosrey@users.noreply.github.com>
2024-03-15 21:45:45 -0500

    Ejercicio con amigos

---
+++ main.js
@@ -0,0 +1,9 @@
+const check = document.getElementById("mayor-de-edad")
+const moto = document.getElementById("moto")
+
+
+check.addEventListener("click", () => {
+    moto.classList.toggle("ocultar");
+})
+
+
